3d4a5dac9b436362ebd7372a396d6ec4959b99be
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-23 12:02:37 -0300

    Create Employee JPA Entity

--- src/main/java/com/buenoezandro/employee/model/Employee.java
+++ src/main/java/com/buenoezandro/employee/model/Employee.java
@@ -15,20 +15,18 @@ import lombok.Data;
 @Entity
 @Table(name = "employees")
 public class Employee implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "first_name", nullable = false)
 	private String firstName;
-	
+
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	@Column(name = "email")
 	private String email;
-
 }
